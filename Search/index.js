b212af0025147e1daa577737b2dfa0b0b3659d7e
jasonn0118 <tlswoals2006@gmail.com>
2021-04-26 22:53:32 -0700

    Search Algorithms

---
+++ Search/index.js
@@ -0,0 +1,17 @@
+//If you know a solution is not far from the root of the tree:
+// A: Breadth First Search
+
+//If the tree is very deep and solutions are rare: 
+// A: BFS(DFS will take long)
+
+//If the tree is very wide:
+// A: Depth First Search (BFS will need to much memory)
+
+//If solutions are frequent but located deep in the tree:
+// A: DFS
+
+//Determining whether a path exists between two nodes:
+// A: DFS
+
+//Finding the shortest path:
+// A: BFS
