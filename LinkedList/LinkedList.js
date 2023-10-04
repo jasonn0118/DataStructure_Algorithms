fed24d536d8b6be086ff4e1078063c6fc3563662
jasonn0118 <tlswoals2006@gmail.com>
2023-10-03 21:26:53 -0700

    recap the singly linked list

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -13,137 +13,132 @@
 // }
 
 class Node {
-    constructor(value){
-      this.value = value,
-      this.next = null
-    }
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
 }
-//Singly linked list: 
+//Singly linked list:
 /**
  * Pros: It needs less memory space than doubly linkedList.
  * Cons: It cannot traverse reversely. (No Backward. If you miss the head node, you are completely lost a list. )
  */
 class LinkedList {
-    //Instanciated.
-    constructor(value) {
-      this.head = {
-        value: value,
-        next: null
-      };
-      this.tail = this.head;
-      this.length = 1;
-    }
-  
-    append(value) {//Add at the end.
-      //Code here
-      const newNode = new Node(value);
-      this.tail.next = newNode;
-      this.tail = newNode;
-      this.length++;
-  
-      return this;
-    }
-  
-    prepend(value) { //Add at the beginning
-      const newNode = new Node(value);
-      newNode.next = this.head;
-      this.head = newNode;
-      this.length++;
-  
-      return this;
-    }
-    //my solution
-    insert(index, value) { //Add at the middle of Nodes.
-      if(index >= this.length) {
-        this.append(value);
-        return this.printList();
-      }
-  
-      const newNode = new Node(value);
-      let aftNode = this.head;
-      let preNode = this.head;
-  
-      for(let i = 0; i < this.length; i++) {
-        if(index === 0) {
-          this.prepend(value);
-          return this.printList();
-        }
-        
-        if(index !== 0 && index === i) {
-          newNode.next = aftNode;
-          preNode.next = newNode;
-          return this.printList();
-        }
-        preNode = aftNode;
-        aftNode = aftNode.next;
-      }
-    }
-    //Solution from the lecture
-    insert1(index, value) { //O(n)
-      if(index >= this.length) {
-        this.append(value);
-        return this.printList();
-      }
-      if(index === 0) {
-          this.prepend(value);
-          return this.printList();
-        }
-      const newNode = new Node(value);
-      const leader = this.traverseToIndex(index-1);
-      const holdingPointer = leader.next;
-  
-      leader.next = newNode;
-      newNode.next = holdingPointer;
-  
-      return this.printList();
-    }
-  
-    traverseToIndex(index) {
-      let counter = 0;
-      let currentNode = this.head;
-      while(counter !== index) {
-        currentNode = currentNode.next;
-        counter++;
-      }
-      return currentNode;
-    }
-    
-    //Print List values only.
-    printList() {
-      const array = [];
-      let currentNode = this.head;
-      while(currentNode !== null) {
-        array.push(currentNode.value);
-        currentNode = currentNode.next;
-      }
-      return array;
-    }
-  
-    remove(index) {
-      if(index === 0) {
-        this.head = this.traverseToIndex(index+1);
-        return this.printList();
-      }
-      if(index >= this.length) {
-        this.tail = this.traverseToIndex(index-1);
-        this.tail.next = null;
-        return this.printList();
-      }
-      const preNode = this.traverseToIndex(index -1);
-      const aftNode = this.traverseToIndex(index +1);
-      preNode.next = aftNode;
-      this.length--;
-      return this.printList();
-    }
-  
+  // Instantiated.
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
   }
-  
-  let myLinkedList = new LinkedList(10);
-  myLinkedList.append(5);
-  myLinkedList.append(16);
-  myLinkedList.prepend(1);
-  myLinkedList.insert1(5, 99);
-  myLinkedList.printList();
-  myLinkedList.remove(2);
-  
-  // console.log(myLinkedList);
+
+  append(value) {
+    //Add at the end.
+    //Code here
+    const newNode = {
+      value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  prepend(value) {
+    //Add at the beginning
+    const newHeadNode = {
+      value,
+      next: this.head,
+    };
+    this.head = newHeadNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const arr = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(arr);
+    return arr;
+  }
+
+  insert(index, value) {
+    // Validation for index is bigger than current linkedList length.
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    let pointer = 0;
+    let currentNode = this.head;
+    while (pointer < index - 1) {
+      currentNode = currentNode.next;
+      pointer++;
+    }
+
+    const newNode = {
+      value,
+      next: currentNode.next,
+    };
+    currentNode.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  // From Lecture solution.
+  insert2(index, value) {
+    // Time complexity: O(n)
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter != index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    // Time complexity: O(n)
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = holdingPointer.next;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(15);
+myLinkedList.append(30);
+myLinkedList.prepend(1);
+myLinkedList.printList();
+myLinkedList.insert2(2, 16);
+myLinkedList.insert2(999, 23);
+myLinkedList.printList();
+myLinkedList.remove(1);
+
+console.log(myLinkedList);
