697593486d00cab74527f4303894e46b46469389
jasonn0118 <tlswoals2006@gmail.com>
2025-01-24 11:43:27 -0800

    refactor Stack implementation to improve push and pop methods and add isEmpty function

--- Array/Stack_implement_in_array.js
+++ Array/Stack_implement_in_array.js
@@ -1,25 +1,31 @@
 class Stack {
-    constructor(){
-      this.array = [];
-    }
-    peek() {
-      return this.array[this.array.length-1];
-    }
-    push(value){
-      this.array.push(value);
-      return this;
-    }
-    pop(){
-      this.array.pop();
-      return this;
-    }
-    //isEmpty
+  constructor() {
+    this.array = [];
   }
-  
-  const myStack = new Stack();
-  myStack.push('google');
-  myStack.push('udemy');
-  myStack.push('Discord');
-  myStack.pop();
-  myStack.peek();
-  
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+  push(value) {
+    this.array = [...this.array, value];
+    return this;
+  }
+  pop() {
+    const lastItem = this.array[this.array.length - 1];
+    this.array = this.array.slice(0, this.array.length - 1);
+    return lastItem;
+  }
+  isEmpty() {
+    if (this.array.length === 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const myStack = new Stack();
+myStack.push("google");
+myStack.push("udemy");
+myStack.push("Discord");
+myStack.pop();
+myStack.peek();
+console.log(myStack);
--- LinkedList/Stack_implement_in_linkedList.js
+++ LinkedList/Stack_implement_in_linkedList.js
@@ -1,57 +1,57 @@
 class Node {
-    constructor(value){
-      this.value = value;
-      this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  class Stack {
-    constructor(){
-      this.top = null;
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  push(value) {
+    var newNode = new Node(value);
+    if (this.length === 0) {
+      this.bottom = newNode;
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    //If we have just one value in the linkedList.
+    if (this.top === this.bottom) {
       this.bottom = null;
-      this.length = 0;
     }
-    peek() {
-      return this.top;
-    }
-    push(value){
-      var newNode = new Node(value);
-      if(this.length === 0) {
-        this.bottom = newNode;
-        this.top = newNode;
-      } else {
-        newNode.next = this.top;
-        this.top = newNode;
-      }
-      this.length++;
-      return this;
-    }
-    pop(){
-      if(!this.top) {
-        return null;
-      }
-      //If we have just one value in the linkedList.
-      if(this.top === this.bottom){
-        this.bottom = null;
-      }
-      //If we want to keep track the target value in memory.
-      //otherwise no way to find this target in JavaScript.
-      const holdingPointer = this.top;
-      this.top = this.top.next;
-      this.length--;
-      
-      return holdingPointer;
-    }
-    isEmpty() {
-      if(this.length === 0){
-        return true;
-      } else return false;
-    }
+    //If we want to keep track the target value in memory.
+    //otherwise no way to find this target in JavaScript.
+    const holdingPointer = this.top;
+    this.top = this.top.next;
+    this.length--;
+
+    return holdingPointer;
   }
-  
-  const myStack = new Stack();
-  myStack.push("Google");
-  myStack.push("Udemy");
-  myStack.push("Discord");
-  myStack.pop();
-  
+  isEmpty() {
+    if (this.length === 0) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const myStack = new Stack();
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("Discord");
+myStack.pop();
