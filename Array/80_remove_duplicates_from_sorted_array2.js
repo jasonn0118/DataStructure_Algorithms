1d5c7b3a9e034c5d80dfa62b8010744c79ed918d
jasonn0118 <tlswoals2006@gmail.com>
2025-03-04 21:36:35 -0800

    add removeDuplicates function to remove duplicates from a sorted array using hash map and two-pointer approaches

---
+++ Array/80_remove_duplicates_from_sorted_array2.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// First Approach - Using hash map
+// Time Complexity: O(n)
+var removeDuplicates = function (nums) {
+  let numbers = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (numbers[nums[i]] === 2) {
+      nums.splice(i, 1);
+      i--;
+    } else if (numbers[nums[i]]) {
+      numbers[nums[i]] += 1;
+    } else {
+      numbers[nums[i]] = 1;
+    }
+  }
+};
+
+// Second Approach - Using two pointers
+// Time Complexity: O(n)
+var removeDuplicates2 = function (nums) {
+  let k = 2;
+
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i] !== nums[k - 2]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
