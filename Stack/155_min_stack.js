6f2c0e71bc95270c1d23ced32a3f10523dee1f54
jasonn0118 <tlswoals2006@gmail.com>
2025-03-12 15:05:14 -0700

    add MinStack class to implement a stack that supports push, pop, top, and retrieving the minimum element in constant time

---
+++ Stack/155_min_stack.js
@@ -0,0 +1,46 @@
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  let min = this.getMin();
+  if (min === null || min > val) {
+    min = val;
+  }
+  this.stack.push([val, min]);
+  console.log(this.stack);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1][0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack.length ? this.stack[this.stack.length - 1][1] : null;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
